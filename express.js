4e78777ece45c6996442e0c507c447efe4aee7bb
Mijamka <1904953@rgu.ac.uk>
2021-03-19 04:29:05 +0000

    server set up and tested

---
+++ express.js
@@ -0,0 +1,28 @@
+//Miriam Wojcik
+//go_sip AB testing website
+//v 2
+//18/3/2020
+
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+import cors from 'cors'
+import helmet from 'helmet'
+
+const __dirname = path.resolve();
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
+app.use(express.static("public"));
+
+export default app
