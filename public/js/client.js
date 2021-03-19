9413d44a2ca229536b07c0fbf3b7637c920cb1e3
Mijamka <1904953@rgu.ac.uk>
2021-03-19 08:29:10 +0000

    Frontend for login page implemented

--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,16 @@
+// Client side scripts
+
 //a function to fetch the category data (button/link id) before posting them to database
 function check_cat(cat){
     $.post('/',{'cat': cat});
   }
+
+//check the category of article before saving the button click in the database
+  function check_cat_art(cat){
+    if(cat == 'social_media'){
+      cat='social media';
+    }
+    $.post('/articles',{'cat': cat});
+  }
+    
+
